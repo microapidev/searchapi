9ab3e276891720de8357e148f3b07b0eb90dcc14
Henry <16284830+code2exe@users.noreply.github.com>
2020-07-25 00:34:45 +0100

    Feat: Minor refactoring for API structure

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,12 +1,11 @@
-from flask import render_template
-from  api import app, db
-from flask import request, jsonify
+from flask import Flask, render_template, jsonify, make_response, Blueprint
+from searchapi import app, db
 import json
 import datetime
 
 
 #Endpoints/routes
-
+api = Blueprint('api', __name__, url_prefix='/api/v1')
     
 
 
