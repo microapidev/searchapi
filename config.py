71f3d2f48c01e64e4a2ed45671217acd77b40eac
Henry <16284830+code2exe@users.noreply.github.com>
2020-07-24 21:08:15 +0100

    Fix: Refactored secrets key generation

--- config.py
+++ config.py
@@ -1,16 +1,18 @@
 import os
+import secrets
 
 #environment variable
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #configuration
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "myseckey12345"
+    SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" +  os.path.join(basedir, "search.db") 
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     #whoosh search 
-    WHOOSH_BASE = 'whoosh' + os.path.join(basedir, "search.db")
+    # WHOOSH_BASE = 'whoosh' + os.path.join(basedir, "search.db")
+    WHOOSH_BASE = 'inde'
     
     
