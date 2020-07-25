f0a9f8d33eb2ef1c4602196e77480593bc295207
KimBitrus26 <bitruschoji4real@gmail.com>
2020-07-25 15:35:06 +0100

    made changes to blueprints and init file

--- src/app/blueprints/webapp/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-webapp = Blueprint('webapp', __name__, template_folder='templates')
-
-@webapp.route('/')
-def index():
-    return render_template('webapp/index.html')
